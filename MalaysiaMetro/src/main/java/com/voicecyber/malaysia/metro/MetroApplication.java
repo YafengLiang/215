13f3197aa5ba7de88a84a714b22cb1fb00065fd4
liangyafeng@voicecodes.com <liangyafeng@vociecodes.com>
2019-05-08 11:04:56 +0800

    Initial commit

---
+++ MalaysiaMetro/src/main/java/com/voicecyber/malaysia/metro/MetroApplication.java
@@ -0,0 +1,17 @@
+package com.voicecyber.malaysia.metro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yafen
+ * @date 2019-05-08
+ */
+@SpringBootApplication
+public class MetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroApplication.class, args);
+	}
+
+}
